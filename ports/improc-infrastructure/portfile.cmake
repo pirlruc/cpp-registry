6c44278a5ebd16e78ce7d405e5e5ab05ab486f38
Nuno Monteiro <pirlruc@gmail.com>
2024-07-24 22:04:28 +0000

    Corrected improc-infrastructure commit

--- ports/improc-infrastructure/portfile.cmake
+++ ports/improc-infrastructure/portfile.cmake
@@ -1,20 +1,31 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO pirlruc/infrastructure
-  REF 34c99d5de87c0f7bb65e935e3f631bd797403f8f
-  SHA512 7e1a1fc1e60970880b34ee781789cc2e0ad25a0c26e37f21bc697cb94697d33155e1f987624b349c2895dcfc68f262716e75cc1176d032f6af53ad5dc92c8cc5
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO pirlruc/infrastructure
+    REF "${VERSION}"
+    SHA512 93ea238237d9af92233702d7567c7ee75fced9de477b61899a69a443f0541193c71a488bcb567b2361858cc92f1a238fe9ae08b62a92a9d3749e64ed3dd7d89c
+    HEAD_REF feature-vcpkg
 )
 
-vcpkg_configure_cmake(
-  SOURCE_PATH "${SOURCE_PATH}"
-  PREFER_NINJA
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tests IMPROC_INFRASTRUCTURE_WITH_TESTS
 )
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
 
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "improc-infrastructure")
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
