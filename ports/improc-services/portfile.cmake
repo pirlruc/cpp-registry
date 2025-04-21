cf0195bfa9705f8074af95e78b094be3ec6c25b8
pirlruc <3945588+pirlruc@users.noreply.github.com>
2025-04-21 10:10:04 +0000

    Update improc-services

--- ports/improc-services/portfile.cmake
+++ ports/improc-services/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pirlruc/services
     REF "${VERSION}"
-    SHA512 0bfbf3c375bf2e69fd3216b89d66ece0bca00d7bb4c8e818cd4a48d934748681ef7314a65d50ce355c0b77e8eee72ecbe36effecc86eaa8e112507daef8b17cc
+    SHA512 84abb726a86d39ff419b2126a7c541f25966dad3956c24a3b8d65bdd1acfc27718b0fef5a0ab3a118436dcf676aef53e4dedc3f8d1022d50e94c5f1771dd29d0
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -26,5 +26,8 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+# configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
